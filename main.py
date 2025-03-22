a2a2843d24f5c1a0e11e401282671af56ea64d76
battosai78 <diazluke91@gmail.com>
2025-03-22 14:12:18 -0400

    Charges

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-"""
-This program calculates prices for custom house signs.
-"""
-
-# Declare and initialize variables here.
-charge = 0
-numChars = 0
-color = 0
-woodType = 0
 
 # Charge for this sign.
+charge = None
 # Number of characters.
+numChars = None
 # Color of characters.
+color = None
 # Type of wood.
+woodType = None
 
 # Write assignment and if statements here as appropriate.
+print("Enter number of characters on sign:")
+numChars = input()
+print("Enter wood type:")
+woodType = input()
+print("Enter color:")
+color = input()
+
+charge = 35
+if int(numChars) > 5:
+    charge = (int(numChars) - 5) * 4 + charge
+if str(woodType) == "oak":
+    charge = charge + 20
+if str(color) == "gold":
+    charge = charge + 15
+
 
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
